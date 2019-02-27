e355ef85c99682ef4875f21b3e230303a4dde245
GuidoSP <stereopt@gmail.com>
2019-02-27 09:31:07 +0000

    Addeed Photon

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 
+const mainWindow = null;
+
 function createWindow() {
-  let window = new BrowserWindow({ width: 800, height: 600 });
-  window.loadFile('index.html');
+  mainWindw = new BrowserWindow({
+    width: 800, height: 600,
+    minWidth: 500, minHeight: 200
+  });
+
+  mainWindw.loadFile('./public/index.html');
 }
 
 console.log("[guiChat]");
 
+app.on('window-all-closed', function() {
+  if(process.platform != 'darwin') app.quit();
+});
+
 app.on('ready', createWindow);
+
+app.on('closed', function() {
+  mainWindow = null;
+});
